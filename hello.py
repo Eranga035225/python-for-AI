dd5edbdde46d023605318213ffdc94b5d7fa50c0
Eranga <erangakavishanka@icloud.com>
2025-11-04 20:55:39 +0530

    strings

--- hello.py
+++ hello.py
@@ -1,13 +1,24 @@
-name = 'Era' # this is a variable
-age = 25
-
-"""
-This is a multi line comment
 
 
 
+text2 = """
+This is a string
+Hello
 """
 
+print(text2)
 
 
-name
+first_name = "Era"
+last_name = "Kavisanka"
+
+full_name  =  first_name + " " + last_name
+
+print(full_name)
+
+long_dash = "-" * 10
+
+len(long_dash)
+
+
+
