333b91ccb3b81905fb5b4fe362a4cd807a359695
Eranga <erangakavishanka@icloud.com>
2025-11-04 21:03:59 +0530

    booleans

--- hello.py
+++ hello.py
@@ -1,24 +1,8 @@
+is_logged_in = True
+
+age = 18
 
 
+can_vote  = age>=18
 
-text2 = """
-This is a string
-Hello
-"""
-
-print(text2)
-
-
-first_name = "Era"
-last_name = "Kavisanka"
-
-full_name  =  first_name + " " + last_name
-
-print(full_name)
-
-long_dash = "-" * 10
-
-len(long_dash)
-
-
-
+can_vote
